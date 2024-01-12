24ebfad0271504aaac935e499e834ace2b43557f
Michael <hnthu.aung@gmail.com>
2024-01-12 13:07:35 +0630

    implementing api token

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+
+class UserController extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+
+            //Validated
+            $validateUser = request()->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json(['message' => 'User Created Successfully',]);
+
+        }
+
+    public function login(Request $request)
+    {
+
+            $validateUser = request()->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'message' => 'Email & Password does not match with our record.',
+                ]);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ]);
+    }
+
+    public function logout(){
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+    }
+}
